70ead00ba5208817060dbf5b102724de596b9c8a
Zoila Rodriguez <zrodriguez@colgate.edu>
2023-10-18 21:47:58 -0400

    fix black reformat issues

--- polls/models.py
+++ polls/models.py
@@ -7,12 +7,12 @@ from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     @admin.display(
         boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
+        ordering="pub_date",
+        description="Published recently?",
     )
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
